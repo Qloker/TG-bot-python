77ff47eb0093facd5602513d3e7086f1b7ce18a1
DESKTOP-R9U6IEU\Ilia Zakharov <98qloker@gmail.com>
2023-03-03 00:50:23 +0300

    ss

--- hw.py
+++ hw.py
@@ -3,4 +3,7 @@ text = 'Never forget what you are, for surely the world will not'
 # BEGIN (write your solution here)
 print(f'''Index of N: {text.find('N')}
 Index of ,: {text.find(',')}''')
+n = '123'
+asd = 'asdwww'
+asda = 'aaaasdasdasd '
 # END 
