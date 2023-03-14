7db8e500bf552bf35ba3a9fb953570844cd256be
DESKTOP-R9U6IEU\Ilia Zakharov <98qloker@gmail.com>
2023-03-15 02:00:32 +0300

    Обновил наполнение БД И функцию получения вещи

--- WTW/BDCloth/bd_clothes.py
+++ WTW/BDCloth/bd_clothes.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 conn = sqlite3.connect('clothing.db')
 cursor = conn.cursor()
@@ -14,15 +15,20 @@ CREATE TABLE IF NOT EXISTS clothing (
 )
 ''')
 
-cursor.execute('''
-INSERT INTO clothing (name, season, temp_min, temp_max, url)
-VALUES 
+data = [
     ("Куртка", "Зима", -30, 0, ["https://kosmo-shop.com/25192/kurtka-raduzhnaya-get-rainbow-mood-raduga-mnogocvetnaya-yarkaya-prikolnaya-veselaya.jpg", "https://framen.ru/d/kurtka-dlinnaja-s-klimat-kontrolem-761-autojack.jpg?h=800", "https://ae01.alicdn.com/kf/H7a65d3f8bf0c4404baf93aa59170366aE.jpg", "https://ae04.alicdn.com/kf/S2ba24b1e2df1447c9b7f5ad891b6834ee.jpg_640x640.jpg"]),
     ("Штаны", "Зима", -30, 0, ["https://kasta.ua/image/1035/s3/5/36/15/9735189/27884299/27884299_original.jpeg"]),
     ("Аксессуар", "Зима", -30, 0, ["https://darvin-market.ru/upload/iblock/0f1/0f1afae5dd8a87086eb74a4fd5494546.jpeg"]),
     ("Футболка", "Зима", -30, 0, ["https://printbar.ru/upload/images/0b/0bb7483.jpg", "https://printbang.ru/img/products/85/muzhskaya-futbolka-pepe-fon-rikardo-milos-fleksonskiy-85979.jpg", "https://bukva-z.ru/upload/thumb/images/go/goh2c1zj0f4_590x0.jpg", "https://printbar.ru/upload/images/mp/mpvtuxi.jpg"])
-''')
+]
+
+for item in data:
+    name, season, temp_min, temp_max, urls = item
+
+    #Таким образом, в поле url будет сохранен список ссылок в формате JSON в виде строки. При чтении данных из БД необходимо будет десериализовать строку JSON обратно в список.
+    url_str = json.dumps(urls)
+    cursor.execute('INSERT INTO clothing (name, season, temp_min, temp_max, url) VALUES (?, ?, ?, ?, ?)', (name, season, temp_min, temp_max, url_str))
 
 conn.commit()
-
 conn.close()
+
--- WTW/Services/getClothes.py
+++ WTW/Services/getClothes.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import random
+import json
 def get_clothing_by_temp(temp):
     conn = sqlite3.connect('clothing.db')
     cursor = conn.cursor()
@@ -9,11 +10,19 @@ def get_clothing_by_temp(temp):
     WHERE temp_min <= ? AND temp_max >= ?
     ''', (temp, temp))
 
-    result = cursor.fetchall()
+    data = cursor.fetchall()
+    result = []
+
+    for i in range(len(data)):
+        result.append((data[i][0], json.loads(data[i][1])))     
 
     conn.close
     return result
 
-tests = get_clothing_by_temp(0)
-print(tests)
-print(tests[0])
+tests = get_clothing_by_temp(-1)
+
+dic_cloth = {}
+
+for i in range(len(tests)):
+    dic_cloth[tests[i][0]] = random.choice(tests[i][1])
+print(dic_cloth)
