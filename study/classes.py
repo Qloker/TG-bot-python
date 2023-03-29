71c91efa76863093eebe951e8988f330b7ee874b
Ilia <bohemawork@gmail.com>
2023-03-29 17:53:57 +0300

    1

--- study/classes.py
+++ study/classes.py
@@ -11,3 +11,14 @@ class Dog():
 test_dog = Dog('ben', 54)
 
 test_dog.sit()
+
+class WildDog(Dog):
+
+    def __init__(self, name, age, sd) -> None:
+
+        super().__init__(name, age)
+        
+        self.sd = sd
+
+new_test = WildDog('a', '231', 'newone')
+print(new_test.sd)
